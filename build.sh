33142c07c9bd99b26798aaa90dc9b476dbccf2d6
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-02-27 00:56:43 +0600

    Not used

--- build.sh
+++ build.sh
@@ -1,17 +1,12 @@
-echo "  _____            _       _   _              __  __ "
-echo " | ____|_   _____ | |_   _| |_(_) ___  _ __   \ \/ / "
-echo " |  _| \ \ / / _ \| | | | | __| |/ _ \| '_ \   \  /  "
-echo " | |___ \ V / (_) | | |_| | |_| | (_) | | | |  /  \  "
-echo " |_____| \_/ \___/|_|\__,_|\__|_|\___/|_| |_| /_/\_\ "
-echo "                                                     "
+echo "  thanks for build infinity x"
 
 ROOT_DIR="$(pwd)"
 cd $ROOT_DIR
 
-EVO_VERSION="$(awk '/EVO_VERSION := / {print $3}' $ROOT_DIR/vendor/lineage/config/version.mk)"
+EVO_VERSION="$(awk '/EVO_VERSION := / {print $3}' $ROOT_DIR/vendor/infinity/config/version.mk)"
 ANDROID_BUILD_VERSION="ap4a"
 
-echo "Building Evolution X version $EVO_VERSION ($ANDROID_BUILD_VERSION)"
+echo "Building infinity X version $EVO_VERSION ($ANDROID_BUILD_VERSION)"
 echo "---------------------------"
 
 source build/envsetup.sh
@@ -27,12 +22,12 @@ compress() {
     echo "----- Compressing the variant -----"
     cd $ROOT_DIR/out/target/product/tdgsi_arm64_ab
     xz -9 -T0 -v -z system.img
-    mv system.img.xz $HOME/Downloads/evolution_arm64_$variant-$EVO_VERSION-unofficial-$RELEASE_DATE.img.xz
+    mv system.img.xz $HOME/Downloads/infinity_arm64_$variant-$EVO_VERSION-unofficial-$RELEASE_DATE.img.xz
 }
 
 build() {
     cd $ROOT_DIR
-    lunch evolution_arm64_$variant-$ANDROID_BUILD_VERSION-userdebug
+    lunch infinity_arm64_$variant-$ANDROID_BUILD_VERSION-userdebug
     make systemimage -j$(nproc --all) || exit
     compress
 }
@@ -60,21 +55,21 @@ echo "Normal size: $normal_size"
 
 echo "----- OTA -----"
 echo "{
-    \"version\": \"$RELEASE_DATE_FMT (Evolution X $EVO_VERSION)\",
+    \"version\": \"$RELEASE_DATE_FMT (infinity $EVO_VERSION)\",
     \"date\": \"$START_TIME\",
     \"variants\": [
         {
-            \"name\": \"evolution_arm64_bgN\",
+            \"name\": \"infinity_arm64_bgN\",
             \"size\": \"$normal_size\",
             \"url\": \"https://github.com/mytja/treble_evo/releases/download/$RELEASE_DATE/evolution_arm64_bgN-$EVO_VERSION-unofficial-$RELEASE_DATE.img.xz\"
         },
         {
-            \"name\": \"evolution_arm64_bgN_slim\",
+            \"name\": \"infinity_arm64_bgN_slim\",
             \"size\": \"$slim_size\",
             \"url\": \"https://github.com/mytja/treble_evo/releases/download/$RELEASE_DATE/evolution_arm64_bgN_slim-$EVO_VERSION-unofficial-$RELEASE_DATE.img.xz\"
         },
         {
-            \"name\": \"evolution_arm64_bvN\",
+            \"name\": \"infinity_arm64_bvN\",
             \"size\": \"$vanilla_size\",
             \"url\": \"https://github.com/mytja/treble_evo/releases/download/$RELEASE_DATE/evolution_arm64_bvN-$EVO_VERSION-unofficial-$RELEASE_DATE.img.xz\"
         }
