25008175a210d92cec639387719dc245cd70febd
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-02-27 00:51:45 +0600

    Aded

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,4 @@
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/evolution_arm64_bgN.mk \
-	$(LOCAL_DIR)/evolution_arm64_bgN_slim.mk \
-	$(LOCAL_DIR)/evolution_arm64_bvN.mk \
+	$(LOCAL_DIR)/infinity_arm64_bgN.mk \
+	$(LOCAL_DIR)/infinity_arm64_bgN_slim.mk \
+	$(LOCAL_DIR)/infinity_arm64_bvN.mk \
