f573bb0f753ba19b4d048004307a5995964b5343
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-02-27 00:50:36 +0600

    Gapps

--- evolution_arm64_bgN_slim.mk
+++ evolution_arm64_bgN_slim.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, device/mytja/evo/device.mk)
 
-PRODUCT_NAME := evolution_arm64_bgN_slim
-PRODUCT_MODEL := Evolution X GSI with Slim GApps
+PRODUCT_NAME := infinity_arm64_bgN_slim
+PRODUCT_MODEL := Infinity X GSI with frn GApps
 
 TARGET_USES_PICO_GAPPS := true
